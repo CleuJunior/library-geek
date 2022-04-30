eb98ee2e2e4a800d5be8264abefc5e18bf688b30
Cleonildo Jr <cleu.junior@gmail.com>
2022-04-30 03:19:33 -0300

    Add new data to the Database

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -13,3 +13,16 @@ INSERT INTO tb_book (title, author, total_pages, price, time_stamp) VALUES ('As 
 INSERT INTO tb_book (title, author, total_pages, price, time_stamp) VALUES ('As Crônicas de Gelo e Fogo - A Tormenta de Espadas', 'George R. R. Martin', 973, 58.23, NOW());
 INSERT INTO tb_book (title, author, total_pages, price, time_stamp) VALUES ('As Crônicas de Gelo e Fogo - O Festim dos Corvos', 'George R. R. Martin', 753, 51.99, NOW());
 INSERT INTO tb_book (title, author, total_pages, price, time_stamp) VALUES ('As Crônicas de Gelo e Fogo - A Dança dos Dragões', 'George R. R. Martin', 1040, 56.78, NOW());
+
+
+
+INSERT INTO tb_client (name, security_number, birth_date, address, phone_number) VALUES ('Luzia Nicole Isabela da Rosa', '573.651.324-26', TIMESTAMP WITH TIME ZONE '1989-07-14', 'Quadra QE 17 Conjunto M', '(61) 3933-5887');
+INSERT INTO tb_client (name, security_number, birth_date, address, phone_number) VALUES ('Calebe Marcelo Mendes', '212.320.546-07', TIMESTAMP WITH TIME ZONE '2000-11-09', 'Rua Aluízio Azevedo', '(51) 2586-9438');
+INSERT INTO tb_client (name, security_number, birth_date, address, phone_number) VALUES ('Bento Gael Caio Rezende', '705.119.613-83', TIMESTAMP WITH TIME ZONE '1999-03-21', 'Passagem Coletiva', '(91) 3899-7311');
+INSERT INTO tb_client (name, security_number, birth_date, address, phone_number) VALUES ('Bento Pedro Henrique Vicente Fogaça', '636.387.089-50', TIMESTAMP WITH TIME ZONE '1987-05-30', 'Rua B Santa Maria', '(79) 2594-3324');
+INSERT INTO tb_client (name, security_number, birth_date, address, phone_number) VALUES ('Fernando Cláudio Sales', '232.764.968-44', TIMESTAMP WITH TIME ZONE '1994-10-29', 'Rua Itaqui Novo Aleixo', '(92) 3678-2944');
+INSERT INTO tb_client (name, security_number, birth_date, address, phone_number) VALUES ('Cauã Francisco Luiz Novaes', '099.278.550-28', TIMESTAMP WITH TIME ZONE '1942-02-19', 'Rua Provenzano Anchieta', '(51) 3795-6231');
+INSERT INTO tb_client (name, security_number, birth_date, address, phone_number) VALUES ('Benjamin José Gael da Cunha', '059.483.578-05', TIMESTAMP WITH TIME ZONE '1982-01-13', 'Rua Uruguai 35', '(51) 3631-1500');
+INSERT INTO tb_client (name, security_number, birth_date, address, phone_number) VALUES ('Heloise Joana Julia Galvão', '778.504.484-80', TIMESTAMP WITH TIME ZONE '1998-01-23', 'Rua Tuerê Jardim Colúmbia', '(67) 3951-2906');
+INSERT INTO tb_client (name, security_number, birth_date, address, phone_number) VALUES ('Manuela Raquel Carla Farias', '187.949.636-49', TIMESTAMP WITH TIME ZONE '1994-11-04', 'Travessa Urbano Eulálio', '(89) 2715-6394');
+INSERT INTO tb_client (name, security_number, birth_date, address, phone_number) VALUES ('Malu Sara Vieira', '417.437.416-34', TIMESTAMP WITH TIME ZONE '1956-02-26', 'Avenida Alda Centro', '(11) 2675-9957');
